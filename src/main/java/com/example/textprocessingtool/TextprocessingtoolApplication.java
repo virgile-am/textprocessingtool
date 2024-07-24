e0397f5768d2f1394f66311ae1328fb70d85aa09
virgile-am <ndayambaje.virgile@amalitechtraining.org>
2024-07-24 11:35:52 +0200

    first commit

---
+++ src/main/java/com/example/textprocessingtool/TextprocessingtoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.textprocessingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextprocessingtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextprocessingtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/textprocessingtool/TextprocessingtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textprocessingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextprocessingtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
