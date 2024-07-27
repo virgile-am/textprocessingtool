b2fd8e6db5ddc74c90ca00f37a2e7b5ccb386abc
virgile-am <ndayambaje.virgile@amalitechtraining.org>
2024-07-27 21:46:53 +0200

    ft text processing

---
+++ src/test/java/com/example/textprocessingtool/service/TextProcessingServiceTest.java
@@ -0,0 +1,2 @@
+package com.example.textprocessingtool.service;public class TextProcessingServiceTest {
+}
---
+++ src/test/java/com/example/textprocessingtool/test/TextProcessingControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.textprocessingtool.controller;
+
+public class TextProcessingControllerTest {
+}
