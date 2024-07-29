a12a6d1329e4c8938bc9db6437cf6b66e0eb04c7
virgile-am <ndayambaje.virgile@amalitechtraining.org>
2024-07-29 10:29:48 +0200

    Update CorsConfig.java

--- src/main/java/com/example/textprocessingtool/config/CorsConfig.java
+++ src/main/java/com/example/textprocessingtool/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.mydomain.websorting.config;
+ package com.example.textprocessingtool.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,8 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/sort")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+
     }
 }
